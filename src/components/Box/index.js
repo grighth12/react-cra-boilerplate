c79771068ebafd2b5f3afdf658fe6f2eefd9768d
grighth12 <grighth12@gmail.com>
2021-10-14 04:37:37 +0900

    emotion 사용을 위한 설정

--- src/components/Box/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Box.css";
-
-const Box = ({ bgColor }) => {
-  return <div className="box" style={{ backgroundColor: bgColor }}></div>;
-};
-
-export default Box;
